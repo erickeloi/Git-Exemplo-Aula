4c9ed4d1e89819dd960501f89f0950a04b06614c
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2022-08-19 10:26:56 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -2,3 +2,8 @@ def tela_inicial():
     print("Tela Inicial")
     print("O que deseja fazer?...")
     #Blocos de código
+
+def tela_login():
+    login_usuario = input("Digite seu nome de login: ")
+    senha_usuario = input("Digite sua senha: ")
+    #Blocos de código
